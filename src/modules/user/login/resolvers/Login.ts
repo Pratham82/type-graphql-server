6bf293d408c48f1389b040bbbf067e5a5562bce4
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-09 23:37:45 +0530

    feat: create login resolver

---
+++ src/modules/user/login/resolvers/Login.ts
@@ -0,0 +1,34 @@
+import { Resolver, Mutation, Arg, Ctx } from 'type-graphql'
+import brcypt from 'bcryptjs'
+import { User } from '../../../../entity/User'
+import { LoginResponse } from 'src/types/Responses'
+import { MyContext } from 'src/types/MyContext'
+
+@Resolver()
+export class LoginResolver {
+  @Mutation(() => User, { nullable: true })
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<User | LoginResponse> {
+    // Validate user
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
+      return { success: false, message: 'User not found' }
+    }
+
+    // Validate password
+    const valid = await brcypt.compare(password, user.password)
+    if (!valid) {
+      return {
+        success: false,
+        message: 'Password does not match with our records',
+      }
+    }
+
+    ctx.req.session.userId = user.id
+
+    return user
+  }
+}
